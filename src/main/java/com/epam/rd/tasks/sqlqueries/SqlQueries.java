d8d8717a6cc2b694e47932e629e79baa4b64778c
thejerome <thejerome@mail.ru>
2020-07-07 13:58:59 +0300

    initial commit

---
+++ src/main/java/com/epam/rd/tasks/sqlqueries/SqlQueries.java
@@ -0,0 +1,54 @@
+package com.epam.rd.tasks.sqlqueries;
+
+/**
+ * Implement sql queries like described
+ */
+public class SqlQueries {
+    //Select all employees sorted by last name in ascending order
+    //language=HSQLDB
+    String select01 = "";
+
+    //Select employees having no more than 5 characters in last name sorted by last name in ascending order
+    //language=HSQLDB
+    String select02 = "";
+
+    //Select employees having salary no less than 2000 and no more than 3000
+    //language=HSQLDB
+    String select03 = "";
+
+    //Select employees having salary no more than 2000 or no less than 3000
+    //language=HSQLDB
+    String select04 = "";
+
+    //Select all employees assigned to departments and corresponding department
+    //language=HSQLDB
+    String select05 = "";
+
+    //Select all employees and corresponding department name if there is one.
+    //Name column containing name of the department "depname".
+    //language=HSQLDB
+    String select06 = "";
+
+    //Select total salary pf all employees. Name it "total".
+    //language=HSQLDB
+    String select07 = "";
+
+    //Select all departments and amount of employees assigned per department
+    //Name column containing name of the department "depname".
+    //Name column containing employee amount "staff_size".
+    //language=HSQLDB
+    String select08 = "";
+
+    //Select all departments and values of total and average salary per department
+    //Name column containing name of the department "depname".
+    //language=HSQLDB
+    String select09 = "";
+
+    //Select lastnames of all employees and lastnames of their managers if an employee has a manager.
+    //Name column containing employee's lastname "employee".
+    //Name column containing manager's lastname "manager".
+    //language=HSQLDB
+    String select10 = "";
+
+
+}
